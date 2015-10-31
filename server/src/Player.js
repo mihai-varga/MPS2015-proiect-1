2aa58c551f10647d6da902fd6bc5981f01bf9000
Georgiana-Diana CIOCÎRDEL <geodiana93@gmail.com>
2015-10-31 15:54:30 +0200

    Adding dictionary keyed by the websocket to keep track of all the clients in the system.

--- server/src/Player.js
+++ server/src/Player.js
@@ -1,8 +1,6 @@
 function Player(ws) {
     // The web socket this client is using.
     this.ws = ws;
-    // The client's UUID.
-    this.uuid = this.generateUuid();
     // Client's name, by default = 'FatCat'.
     this.name = 'FatCat';
     // Client's current score.
@@ -19,18 +17,6 @@ Player.prototype.setDice = function(dices) {
 // Will update the client's score, based on the word the client has provided.
 Player.prototype.computeScore = function(word) {
     // TODO: add implementation here
-}
-
-// Generates a UUID as per http://www.ietf.org/rfc/rfc4122.txt.
-Player.prototype.generateUuid = function() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-            function(c) {
-                var r = (d + Math.random() * 16) % 16 | 0;
-                d = Math.floor(d/16);
-                return (c=='x' ? r : (r&0x3 | 0x8)).toString(16);
-    });
-    return uuid;
 }
 
 // Increments current round.
--- server/src/Server.js
+++ server/src/Server.js
@@ -1,13 +1,17 @@
 var WebSocketServer = require('ws').Server
     , wss = new WebSocketServer({ port: 8080});
 
-wss.on('connection', function connection(ws) {
-    var newClient = new Player(ws);
+// A key-value dictionary to keep track of the connected clients. The key will
+// be the web socket the client is using to communicate with the server.
+var allPlayers = {};
 
-    console.log('Client #%s, %s connected.', newClient.uuid, newClient.name);
+wss.on('connection', function connection(ws) {
+    // Add a new player to the dictionary.
+    allPlayers[ws] = new Player(ws);
+    console.log('Client %s connected.', allPlayers[ws].name);
 
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+        console.log('received: %s from %s', message, allPlayers[ws].name);
     });
 
     ws.send('something', function ack(error) {
@@ -15,6 +19,9 @@ wss.on('connection', function connection(ws) {
     });
 
     ws.on('close', function() {
-        // TODO: Add code here.
+        // Disconnecting a client means deleting the client's entry from the
+        // dictionary.
+        console.log('Client %s disconnected.', allPlayers[ws].name);
+        delete allPlayers[ws];
     });
 });
