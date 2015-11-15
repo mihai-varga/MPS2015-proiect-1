b793c5c322d966ef8fec061798c13f8429be5ae8
Alexandru Alistar <alexandru.alistar@cti.pub.ro>
2015-11-15 21:44:10 +0200

    Logica de punctare

--- server/src/Player.js
+++ server/src/Player.js
@@ -17,6 +17,17 @@ Player.prototype.setDice = function(dices) {
 // Will update the client's score, based on the word the client has provided.
 Player.prototype.computeScore = function(word) {
     // TODO: add implementation here
+	if (word.length < 4) {
+		this.score = 0;
+	}
+	if (word.length > 3 && word.length < 10) {
+		this.score = word.length;
+	// BONUS: When the word's length is greater than 5, then the score will be
+	// the length's square 
+		if (word.length > 5) {
+			this.score = word.length * word.length;
+		}
+	}
 }
 
 // Increments current round.
